cf45bd1339acc8f23f1e529ae35d55640c8618b3
Bill D <355438+vegetabill@users.noreply.github.com>
2020-04-22 14:27:49 -0700

    add API tests

--- test/basic-api-test.js
+++ test/basic-api-test.js
@@ -1,5 +1,6 @@
 const { Server } = require('crudtastic');
 const request = require('supertest');
+const should = require('should');
 
 describe('crudtastic REST API', () => {
   let app;
@@ -17,5 +18,37 @@ describe('crudtastic REST API', () => {
 
   it('GET books index', () => {
     return app.get('/books').expect('Content-Type', /json/).expect(200);
+  });
+
+  it('DELETE books/1', () => {
+    return app
+      .delete('/books/1')
+      .expect(200)
+      .then(() => {
+        return app.get('/books/1').expect(404);
+      });
+  });
+
+  it('POST /books adds new book', () => {
+    const book = {
+      title: "Osito's Day Out",
+      publicationDate: '2020-01-01T08:00:00.000Z',
+      authorId: 2,
+      coverImageUrl: 'http://bonito.org/book-cover.jpg',
+      summary: 'A good read'
+    };
+    return app
+      .post('/books')
+      .send(book)
+      .expect(201)
+      .then((resp) => {
+        const { body } = resp;
+        body.should.containEql(book);
+        return app.get(`/books/${body.id}`).expect(200);
+      });
+  });
+
+  after(() => {
+    process.exit(0);
   });
 });
